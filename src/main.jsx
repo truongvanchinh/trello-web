07b2e69eae4d1ef262a28e208c446378024af013
unknown <chinhtv124@gmail.com>
2024-10-19 11:41:16 +0700

    add redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import App from '~/App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
-import theme from '~/theme.js'
+import theme from '~/theme'
 
 //cấu hình react-toastify
 import { ToastContainer } from 'react-toastify'
@@ -11,20 +11,26 @@ import 'react-toastify/dist/ReactToastify.css'
 //Dialog confirm
 import { ConfirmProvider } from 'material-ui-confirm'
 
+//Redux
+import { Provider } from 'react-redux'
+import store from '~/redux/store'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <CssVarsProvider theme={theme}>
-    <ConfirmProvider defaultOptions={{
-      allowClose: false,
-      dialogProps: { maxWidth: 'xs' },
-      cancellationButtonProps: { variant: 'outlined' },
-      confirmationButtonProps: { variant: 'contained' },
-      buttonOrder: ['confirm', 'cancel']
-    }}>
-      <CssBaseline />
-      <App />
-      <ToastContainer />
-    </ConfirmProvider>
-  </CssVarsProvider>
+  <Provider store={store}>
+    <CssVarsProvider theme={theme}>
+      <ConfirmProvider defaultOptions={{
+        allowClose: false,
+        dialogProps: { maxWidth: 'xs' },
+        cancellationButtonProps: { variant: 'outlined' },
+        confirmationButtonProps: { variant: 'contained' },
+        buttonOrder: ['confirm', 'cancel']
+      }}>
+        <CssBaseline />
+        <App />
+        <ToastContainer />
+      </ConfirmProvider>
+    </CssVarsProvider>
+  </Provider>
   // </React.StrictMode>
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,8 @@
-// Redux: State management tool
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from '~/redux/authSlice'
+const store = configureStore({
+  reducer: {
+    auth: authReducer
+  }
+})
+export default store
