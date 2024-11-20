c701dd001bccfc83e08d34ce77694237ee3ffab8
unknown <chinhtv124@gmail.com>
2024-11-20 16:48:47 +0700

    lesson 12: get list board

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,6 @@ if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'https://trello-api-7149.onrender.com'
 }
 export const API_ROOT = apiRoot
+
+export const DEFAULT_PAGE = 1
+export const DEFAULT_ITEMS_PER_PAGE = 12
